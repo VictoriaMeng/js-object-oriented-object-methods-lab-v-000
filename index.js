df8b1d64465cd40a8ec5794b43fa50c26e7d2429
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 18:04:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ function BoardMember(name, homeState, training) {
   this.veto = function veto() {return "No, I must disagree"};
   this.approve = function approve() {return "You can do that!"};
   this.doCharity = function doCharity() {return "I like to help people."};
+  this.releasePressStatement = function releasePressStatement() {return "You will see great things from Scuber."};
 };
